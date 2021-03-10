5362f1c7b01fea0944490ec787b5d3a687f16951
Sergei Tsarik <sergei.tsarik@thomsonreuters.com>
2021-03-10 16:14:33 +0300

    add

---
+++ src/main/java/giftbox/GiftBox.java
@@ -0,0 +1,85 @@
+Copyright © 2020 EPAM Systems, Inc. All Rights Reserved. All information contained herein is, and remains the
+property of EPAM Systems, Inc. and/or its suppliers and is protected by international intellectual
+property law. Dissemination of this information or reproduction of this material is strictly forbidden,
+unless prior written permission is obtained from EPAM Systems, Inc
+
+package giftbox;
+
+import candies.Candy;
+import util.Finding;
+import util.Sorting;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Class Description:
+ * Implement interfaces: Sorting and Finding and
+ * their methods for sorting candies by name or weight and
+ * finding candies by parameters.
+ * <p>
+ * Created by sergei_tsarik on 12/21/20.
+ */
+public class GiftBox
+{
+    /**
+     * No actions are required for class variable candiesList.
+     */
+    private List<Candy> candiesList;
+
+    /**
+     * No actions are required for constructor GiftBox().
+     */
+    public GiftBox()
+    {
+        candiesList = new ArrayList<Candy>();
+    }
+
+    /**
+     * No actions are required for method addCandy().
+     */
+    public void addCandy(Candy candy)
+    {
+        this.candiesList.add(candy);
+    }
+
+    /**
+     * Implement sorting of candiesList by its names in ascending order
+     * and return sorted by name ascending list of candies.
+     */
+    @Override
+    public List<Candy> sortCandiesByNameAsc()
+    {
+        throw new UnsupportedOperationException("You need to implement this method");
+    }
+
+    /**
+     * Implement sorting of candiesList by its weight in ascending order
+     * and return sorted by weight ascending list of candies.
+     */
+    @Override
+    public List<Candy> sortCandiesByWeightAsc()
+    {
+        throw new UnsupportedOperationException("You need to implement this method");
+    }
+
+    /**
+     * Implement selection of candies from candiesList with names started with candyNameFirstLetter
+     * and return list of candies which names started with candyNameFirstLetter in name ascending list of candies.
+     */
+    @Override
+    public List<Candy> findCandiesByNameStartedWith(char candyNameFirstLetter)
+    {
+        throw new UnsupportedOperationException("You need to implement this method");
+    }
+
+    /**
+     * Implement selection ot candies from candiesList so that its [weightFrom <= weight => weightTo]
+     * and return list of candies with weight in range [weightFrom, weightTo] in weight ascending list of candies.
+     */
+    @Override
+    public List<Candy> findCandiesByWeightInRange(int weightFrom, int weightTo)
+    {
+        throw new UnsupportedOperationException("You need to implement this method");
+    }
+}
